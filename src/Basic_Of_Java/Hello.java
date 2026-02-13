3cd19d5ba36a7c8ca39c0664e6479cb987b91c47
Mohd Aamir <aamirgada02@gmail.com>
2026-02-13 19:17:07 +0530

    DSA START

---
+++ src/Basic_Of_Java/Hello.java
@@ -0,0 +1,8 @@
+package Basic_Of_Java;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello DSA");
+    }
+
+}
---
+++ src/Basic_Of_Java/PrintingNumbers.java
@@ -0,0 +1,11 @@
+package Basic_Of_Java;
+
+public class PrintingNumbers {
+
+    public static void main(String[] args) {
+
+        System.out.println(69+1);
+        System.out.println("69+1");
+
+    }
+}
