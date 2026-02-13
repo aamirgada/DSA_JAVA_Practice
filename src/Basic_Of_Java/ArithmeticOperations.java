638dc24e467f16b8aed91ed820dfde030015b46f
Mohd Aamir <aamirgada02@gmail.com>
2026-02-13 19:30:28 +0530

    Variable and Arithmetic intro

---
+++ src/Basic_Of_Java/ArithmeticOperations.java
@@ -0,0 +1,19 @@
+package Basic_Of_Java;
+
+public class ArithmeticOperations {
+
+    static void main(String[] args) {
+        double  x=33;
+        double y= 15;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+
+
+
+
+
+    }
+}
--- src/Basic_Of_Java/VariablesBasics.java
+++ src/Basic_Of_Java/VariablesBasics.java
@@ -1,4 +1,19 @@
 package Basic_Of_Java;
 
+import java.sql.SQLOutput;
+
 public class VariablesBasics {
+
+    static void main(String[] args) {
+        int x = 5;
+        System.out.println(x);
+
+        int y; //declaration
+        y=20; //initialization
+
+        System.out.println(y);
+        System.out.println(x+y);
+
+
+    }
 }
